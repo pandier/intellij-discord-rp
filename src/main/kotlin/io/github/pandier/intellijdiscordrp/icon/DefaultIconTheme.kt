4be6d77e8b4918835e99e4fdaad4f81aaef90455
Pandier <firegox@email.cz>
2024-03-22 19:49:48 +0100

    feat: Add application icon fallback

--- src/main/kotlin/io/github/pandier/intellijdiscordrp/icon/DefaultIconTheme.kt
+++ src/main/kotlin/io/github/pandier/intellijdiscordrp/icon/DefaultIconTheme.kt
@@ -5,13 +5,14 @@ object DefaultIconTheme : IconTheme {
     private const val FILE_ICON_REPOSITORY = "$ICON_REPOSITORY/file"
     private const val APPLICATION_ICON_REPOSITORY = "$ICON_REPOSITORY/application"
 
+    private const val APPLICATION_FALLBACK = "$APPLICATION_ICON_REPOSITORY/fallback.png"
     private val applicationMapping = mapOf(
         "ic" to "$APPLICATION_ICON_REPOSITORY/ij.png",
         "iu" to "$APPLICATION_ICON_REPOSITORY/ij.png",
         "ie" to "$APPLICATION_ICON_REPOSITORY/ij.png",
     )
 
-    private const val FILE_FALLBACK = "$ICON_REPOSITORY/file.png"
+    private const val FILE_FALLBACK = "$FILE_ICON_REPOSITORY/fallback.png"
     private val fileMapping = mapOf(
         "java" to "$FILE_ICON_REPOSITORY/java.png",
         "rust" to "$FILE_ICON_REPOSITORY/rust.png",
@@ -21,7 +22,7 @@ object DefaultIconTheme : IconTheme {
     )
 
     override fun getApplication(productCode: String): String =
-        applicationMapping[productCode.lowercase()]!!
+        applicationMapping[productCode.lowercase()] ?: APPLICATION_FALLBACK
 
     override fun getFile(type: String): String =
         fileMapping[type.lowercase()] ?: FILE_FALLBACK
