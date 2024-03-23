aa227cbab58aa009b1a21d0414eaed4f79381d42
Pandier <firegox@email.cz>
2024-03-23 14:09:57 +0100

    feat: Use commit hash for icon repository; closes #1

--- src/main/kotlin/io/github/pandier/intellijdiscordrp/activity/icon/IconRepository.kt
+++ src/main/kotlin/io/github/pandier/intellijdiscordrp/activity/icon/IconRepository.kt
@@ -1,5 +1,19 @@
 package io.github.pandier.intellijdiscordrp.activity.icon
 
-const val ICON_REPOSITORY = "https://raw.githubusercontent.com/pandier/intellij-discord-rp/main/icons"
+/*
+ * HOW TO UPDATE ICON REPOSITORY
+ *
+ * The icon repository uses a specific git commit hash to avoid incompability
+ * between plugin versions. If an icon is added, modified or removed,
+ * commit these changes. Then copy the hash of the commit and paste it
+ * into ICON_REPOSITORY_COMMIT constant. Commit this new change as:
+ *
+ * chore: Update icon repository
+ *
+ * When these changes make it into the repository, the icons
+ * will start displaying correctly.
+ */
+private const val ICON_REPOSITORY_COMMIT = "41f718b48aed30cf24db82601792054b4e36e80a"
+const val ICON_REPOSITORY = "https://raw.githubusercontent.com/pandier/intellij-discord-rp/$ICON_REPOSITORY_COMMIT/icons"
 const val FILE_ICON_REPOSITORY = "$ICON_REPOSITORY/file"
 const val APPLICATION_ICON_REPOSITORY = "$ICON_REPOSITORY/application"
