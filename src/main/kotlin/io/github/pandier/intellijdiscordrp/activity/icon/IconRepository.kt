67b40eb71db3f1502b4b56d5e0a5dad7fe3d4ac5
Pandier <firegox@email.cz>
2024-03-24 14:56:02 +0100

    chore: Update icon repository

--- src/main/kotlin/io/github/pandier/intellijdiscordrp/activity/icon/IconRepository.kt
+++ src/main/kotlin/io/github/pandier/intellijdiscordrp/activity/icon/IconRepository.kt
@@ -13,7 +13,7 @@ package io.github.pandier.intellijdiscordrp.activity.icon
  * When these changes make it into the repository, the icons
  * will start displaying correctly.
  */
-private const val ICON_REPOSITORY_COMMIT = "41f718b48aed30cf24db82601792054b4e36e80a"
+private const val ICON_REPOSITORY_COMMIT = "cdf95222b37ed59cf8980484950f280dbf3776e4"
 const val ICON_REPOSITORY =
     "https://raw.githubusercontent.com/pandier/intellij-discord-rp/$ICON_REPOSITORY_COMMIT/icons"
 const val FILE_ICON_REPOSITORY = "$ICON_REPOSITORY/file"
