cdf95222b37ed59cf8980484950f280dbf3776e4
Pandier <firegox@email.cz>
2024-03-24 14:55:26 +0100

    feat: Add more file icons; closes #3

--- src/main/kotlin/io/github/pandier/intellijdiscordrp/activity/ActivityFileType.kt
+++ src/main/kotlin/io/github/pandier/intellijdiscordrp/activity/ActivityFileType.kt
@@ -9,11 +9,26 @@ enum class ActivityFileType(
     val extensions: Set<String> = hashSetOf(),
     private val iconFile: String = "fallback.png",
 ) {
+    C("C", "c", hashSetOf("c"), "c.png"),
+    CPP("C++", "c++", hashSetOf("cpp", "h", "hpp", "tcc"), "cpp.png"),
+    CSHARP("C#", "c#", hashSetOf("cs"), "csharp.png"),
+    CSS("CSS", "css", hashSetOf("css"), "css.png"),
+    GO("Go", "go", hashSetOf("go"), "go.png"),
+    GRADLE("Gradle", "gradle", hashSetOf("gradle"), "gradle.png"),
+    GROOVY("Groovy", "groovy", hashSetOf("groovy", "gy"), "groovy.png"),
+    HTML("HTML", "html", hashSetOf("htm", "html", "xhtm", "xhtml"), "html.png"),
     JAVA("Java", "java", hashSetOf("java"), "java.png"),
+    JAVASCRIPT("JavaScript", "javascript", hashSetOf("js", "jsx"), "javascript.png"),
+    JSON("JSON", "json", hashSetOf("json", "jsonc"), "json.png"),
     KOTLIN("Kotlin", "kotlin", hashSetOf("kt", "kts"), "kotlin.png"),
+    MARKDOWN("Markdown", "markdown", hashSetOf("md", "mdx"), "markdown.png"),
+    PHP("PHP", "php", hashSetOf("php"), "php.png"),
+    PYTHON("Python", "python", hashSetOf("py"), "python.png"),
     RUST("Rust", "rust", hashSetOf("rs"), "rust.png"),
-    JAVASCRIPT("JavaScript", "javascript", hashSetOf("js"), "javascript.png"),
+    TOML("TOML", "toml", hashSetOf("toml"), "toml.png"),
     TYPESCRIPT("TypeScript", "typescript", hashSetOf("ts"), "typescript.png"),
+    XML("XML", "xml", hashSetOf("xml"), "xml.png"),
+    YAML("YAML", "yaml", hashSetOf("yml", "yaml"), "yaml.png"),
     OTHER("File");
 
     val icon: String
