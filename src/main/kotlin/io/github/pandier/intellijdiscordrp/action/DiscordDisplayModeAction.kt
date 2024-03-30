1e2bd2f65260b0f33f1b7c7c1dcb084cf93f4136
Pandier <firegox@email.cz>
2024-03-30 21:13:27 +0100

    feat: Make display mode action toggle action

--- src/main/kotlin/io/github/pandier/intellijdiscordrp/action/DiscordDisplayModeAction.kt
+++ src/main/kotlin/io/github/pandier/intellijdiscordrp/action/DiscordDisplayModeAction.kt
@@ -1,26 +1,31 @@
 package io.github.pandier.intellijdiscordrp.action
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareToggleAction
 import io.github.pandier.intellijdiscordrp.activity.ActivityDisplayMode
-import io.github.pandier.intellijdiscordrp.service.discordService
 import io.github.pandier.intellijdiscordrp.settings.project.discordProjectSettingsComponent
 
 abstract class DiscordDisplayModeAction(
     private val displayMode: ActivityDisplayMode,
-) : AnAction() {
+) : DumbAwareToggleAction() {
 
     override fun getActionUpdateThread(): ActionUpdateThread =
         ActionUpdateThread.BGT
 
+
     override fun update(e: AnActionEvent) {
+        super.update(e)
         e.presentation.isEnabledAndVisible = e.project != null
     }
 
-    override fun actionPerformed(e: AnActionEvent) {
-        e.project?.discordProjectSettingsComponent?.state?.displayMode = displayMode
-        discordService.updateActivity()
+    override fun isSelected(e: AnActionEvent): Boolean =
+        e.project?.discordProjectSettingsComponent?.state?.displayMode == displayMode
+
+    override fun setSelected(e: AnActionEvent, state: Boolean) {
+        if (state) {
+            e.project?.discordProjectSettingsComponent?.state?.displayMode = displayMode
+        }
     }
 }
 
