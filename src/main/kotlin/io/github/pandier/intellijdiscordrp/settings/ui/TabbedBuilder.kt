21542ad442589b374afe316099cb3f89ea3a9fb5
Pandier <firegox@email.cz>
2024-04-14 09:59:35 +0200

    refactor: Remove experimental tab header api

--- src/main/kotlin/io/github/pandier/intellijdiscordrp/settings/ui/TabbedBuilder.kt
+++ src/main/kotlin/io/github/pandier/intellijdiscordrp/settings/ui/TabbedBuilder.kt
@@ -1,9 +1,11 @@
 package io.github.pandier.intellijdiscordrp.settings.ui
 
+import com.intellij.ide.ui.laf.darcula.ui.DarculaTabbedPaneUI
 import com.intellij.ui.components.JBTabbedPane
-import com.intellij.ui.dsl.builder.Cell
 import com.intellij.ui.dsl.builder.Panel
 import com.intellij.ui.layout.ComponentPredicate
+import java.awt.Dimension
+import javax.swing.JPanel
 
 fun Panel.tabbed(init: TabbedBuilder.() -> Unit) =
     TabbedBuilder().also(init).finish(this)
@@ -18,18 +20,50 @@ class TabbedBuilder {
     }
 
     fun finish(panel: Panel) {
-        lateinit var tabbedPane: Cell<JBTabbedPane>
+        val tabbedHeader = TabbedHeader()
+        tabTitles.forEach(tabbedHeader::addTab)
 
         // Add a tabbed pane header to the panel
         panel.row {
-            @Suppress("UnstableApiUsage")
-            tabbedPane = tabbedPaneHeader(tabTitles)
+            cell(tabbedHeader)
         }
 
         // Put the tab contents as seperate indents visible only if the tab is selected
         for ((index, builder) in tabBuilders.withIndex()) {
-            panel.indent(builder).visibleIf(TabPredicate(tabbedPane.component, index))
+            panel.indent(builder).visibleIf(TabPredicate(tabbedHeader, index))
         }
+    }
+}
+
+private class TabbedHeader : JBTabbedPane() {
+    override fun updateUI() {
+        setUI(UI())
+    }
+
+    override fun getPreferredSize(): Dimension {
+        val insets = insets
+        val ui = ui as UI
+        return Dimension(
+            ui.getWidth() + insets.right + insets.left,
+            ui.getHeight() + insets.top + insets.bottom,
+        )
+    }
+
+    override fun getMinimumSize(): Dimension =
+        preferredSize
+
+    fun addTab(title: String) =
+        addTab(title, JPanel())
+
+    class UI : DarculaTabbedPaneUI() {
+        fun getWidth(): Int {
+            val metrics = tabPane.getFontMetrics(tabPane.font)
+            return (0 until tabPane.tabCount)
+                .sumOf { calculateTabWidth(tabPane.tabPlacement, it, metrics) }
+        }
+
+         fun getHeight(): Int =
+             calculateMaxTabHeight(tabPane.tabPlacement)
     }
 }
 
