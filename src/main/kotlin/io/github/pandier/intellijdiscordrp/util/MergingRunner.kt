733e74897e3af94560215ce8f26b498aa87f1d3f
Pandier <firegox@email.cz>
2024-05-02 13:02:08 +0200

    docs: Fix merging runner documentation

--- src/main/kotlin/io/github/pandier/intellijdiscordrp/util/MergingRunner.kt
+++ src/main/kotlin/io/github/pandier/intellijdiscordrp/util/MergingRunner.kt
@@ -2,11 +2,10 @@ package io.github.pandier.intellijdiscordrp.util
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 /**
- * A runner that allows only one task to be running at a time and merges any new tasks
- * with the existing one. Tasks will be launched in a couroutine with the specified [scope] and [context].
+ * A runner that allows only one task to be running at a time and merges
+ * any new tasks with the existing one.
  *
  * This class is thread-safe.
  *
@@ -19,10 +18,9 @@ class MergingRunner<T> {
     /**
      * Runs the [task] or merges with an already running task.
      *
-     * If the previous task already finished (or there was no previous task),
-     * the given task will be launched in a new coroutine with the [scope] and [context] specified in the constructor
-     * and the result will be returned. If the previous task didn't finish yet,
-     * it will wait and return the result of that task.
+     * If the previous task already finished (or there was no previous task), the given task
+     * will be launched in the current thread and the result will be returned. If the previous
+     * task didn't finish yet, it will wait and return the result of that task.
      */
     suspend fun run(task: suspend () -> T): Deferred<T> {
         mutex.lock()
